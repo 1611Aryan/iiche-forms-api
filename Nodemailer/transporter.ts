57f64669be395f86160cf2651cb8a14479eb0915
aryan <aryan2563737@gmail.com>
2022-01-10 04:45:57 +0530

    Code Cleanup

--- Nodemailer/transporter.ts
+++ Nodemailer/transporter.ts
@@ -1,15 +1,6 @@
 import nodemailer from "nodemailer"
 process.env.NODE_ENV !== "production" && require("dotenv").config()
 
-console.log({
-  type: "OAUTH2",
-  user: process.env.GMAIL_USERNAME,
-  clientId: process.env.OAUTH_CLIENT_ID,
-  clientSecret: process.env.OAUTH_CLIENT_SECRET,
-  refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-  accessToken: process.env.OAUTH_ACCESS_TOKEN,
-})
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
