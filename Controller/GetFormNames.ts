de48663e8ac0da0dfe2716a01863fbab69491d5b
aryan <aryan2563737@gmail.com>
2022-01-18 03:07:39 +0530

    Get Forms API Change

--- Controller/GetFormNames.ts
+++ Controller/GetFormNames.ts
@@ -3,7 +3,7 @@ import Form, { formI } from "../Models/Form.model"
 
 const GetForms = async (req: Request, res: Response) => {
   try {
-    const forms = (await Form.find({}, { formName: 1 }).lean()) || []
+    const forms = (await Form.find({}, { formName: 1, active: 1 }).lean()) || []
     return res.status(200).send({ forms })
   } catch (err) {
     console.log({ GetForms: err })
